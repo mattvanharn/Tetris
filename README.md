c9227a24addbb611924f314c32ddc5f648fbeef2
Matt Van Harn <mattvanharn@gmail.com>
2022-10-17 17:23:45 -0400

    Add tetromino shapes

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# Tetris
+# Basic Tetris
 This is a Tetris project to practice JavaScript. It was made by following this guide https://www.youtube.com/watch?v=rAUn1Lom6dw&amp;t=2261s
+
---
+++ app.js
@@ -0,0 +1,61 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const grid = document.querySelector('.grid');
+  let squares = Array.from(document.querySelectorAll('.grid div'));
+  const ScoreDisplay = document.querySelector('#score');
+  const StartBtn = document.querySelector('#start-button');
+  const width = 10;
+
+  //The tetrominoes
+  const lTetromino = [
+    [1, width + 1, width * 2 + 1, 2],
+    [width, width + 1, width + 2, width * 2 + 2],
+    [1, width + 1, width * 2, width * 2 + 1],
+    [width, width * 2, width * 2 + 1, width * 2 + 2]
+  ];
+
+  const zTetronimo = [
+    [width + 1, width + 2, width * 2, width * 2 + 1],
+    [0, width, width + 1, width * 2 + 1],
+    [width + 1, width + 2, width * 2, width * 2 + 1],
+    [0, width, width + 1, width * 2 + 1]
+  ];
+
+  const tTetronimo = [
+    [1, width, width + 1, width + 2],
+    [1, width + 1, width + 2, width * 2 + 1],
+    [width, width + 1, width + 2, width * 2 + 1],
+    [1, width, width + 1, width * 2 + 1]
+  ];
+
+  const oTetronimo = [
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1]
+  ];
+
+  const iTetronimo = [
+    [1, width + 1, width * 2 + 1, width * 3 + 1],
+    [width, width + 1, width + 2, width + 3],
+    [1, width + 1, width * 2 + 1, width * 3 + 1],
+    [width, width + 1, width + 2, width + 3]
+  ];
+
+  const theTetrominoes = [lTetromino, zTetronimo, tTetronimo, oTetronimo, iTetronimo];
+
+  let currentPosition = 4;
+
+  //select a random tetromino
+  let current = theTetrominoes[0][0];
+
+  //draw the first rotation in the first tetromino
+  function draw() {
+    current.forEach(index => {
+      squares[currentPosition + index].classList.add('tetromino')
+    })
+  }
+
+  draw();
+
+
+})
